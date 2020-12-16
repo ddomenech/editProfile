2418c321d8ca5746e7d9bd949297026310364dce
daviddomenech <daviddomenech@cecotec.es>
2020-12-16 20:11:09 +0100

    include  requirements

--- editProfile/urls.py
+++ editProfile/urls.py
@@ -1,38 +1,22 @@
-"""editProfile URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.conf import settings
+from django.urls import include, path
 from django.conf.urls.static import static
-from extupf.views import UserViewSet, UserProfileViewSet, GetUserProfileView
-from django.urls import path, include
-from rest_framework.documentation import  include_docs_urls
+from django.contrib import admin
+from rest_framework.documentation import include_docs_urls
 from rest_framework.routers import DefaultRouter
-from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token 
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
+
+from extupf.views import UserProfileViewSet
 
 router = DefaultRouter()
-router.register(r'users',UserViewSet)
-router.register(r'profile', UserProfileViewSet)
+router.register(r'profiles', UserProfileViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(router.urls)),
-    #path('upload/<str:filename>/', CargarArchivoView.as_view(), name="upload"),
     path('api/v1/api-token-auth/', obtain_jwt_token),
     path('api/v1/api-token-refresh/', refresh_jwt_token),
-    path('api/v1/api-auth/',include('rest_framework.urls')),
+    path('api/v1/api-auth/', include('rest_framework.urls')),
     path('api/v1/docs', include_docs_urls(title='Mi Api')),
 ]
 
--- extupf/views.py
+++ extupf/views.py
@@ -1,83 +1,49 @@
-from django.shortcuts import render
-from rest_framework.views import APIView
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework import status, viewsets, mixins
-from rest_framework.parsers import FileUploadParser
-from rest_framework.exceptions import ParseError
-from rest_framework_jwt.authentication import JSONWebTokenAuthentication
-from rest_framework import generics
-from rest_framework.response import Response
-from rest_framework.decorators import action
-from .serializers import UserProfileSerializer, UserSerializer, PasswordSerializer, AvatarSerializer
-from .models import UserProfile
-from django.contrib.auth.models import User
-from rest_framework.request import Request
-
 from rest_framework import permissions
-from .permissions import IsOwnerOrReadOnly, IsSameUserAllowEditionOrReadOnly, IsAdminUserOrReadOnly, IsOwner
+from rest_framework import status, viewsets, mixins
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import UserProfile
+from .permissions import IsOwnerOrReadOnly, IsSameUserAllowEditionOrReadOnly, \
+    IsOwner
+from .serializers import UserProfileSerializer, UserSerializer, \
+    PasswordSerializer
+
 
 class UserProfileViewSet(mixins.ListModelMixin,
-                  mixins.RetrieveModelMixin,
-                  mixins.UpdateModelMixin,
-                  viewsets.GenericViewSet):
-    
-    queryset=UserProfile.objects.all()
-    serializer_class=UserProfileSerializer
+                         mixins.RetrieveModelMixin,
+                         mixins.UpdateModelMixin,
+                         viewsets.GenericViewSet):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
     permission_classes = (IsOwner,)
-    
+
     def get_queryset(self):
-        """
-        This view should return a list of all the purchases
-        for the currently authenticated user.
-        """
         user = self.request.user
         return UserProfile.objects.filter(user_id=user.id)
 
-    @action(detail=True,methods=['put','patch'], serializer_class=PasswordSerializer)
+    @action(detail=True, methods=['put', 'patch'], serializer_class=PasswordSerializer)
     def set_password(self, request, pk):
-        serializer =  PasswordSerializer(data=request.data)
+        serializer = PasswordSerializer(data=request.data)
         user = request.user
         if serializer.is_valid():
             if not user.check_password(serializer.data.get('old_password')):
-                return Response({"old_password": ["wrong password."]}, status=status.HTTP_400_BAD_REQUEST)
+                return Response({"old_password": ["wrong password."]},
+                                status=status.HTTP_400_BAD_REQUEST)
             user.set_password(serializer.data.get('new_password'))
             user.save()
-            return Response({"status":"password set"}, status=status.HTTP_200_OK)
-        
+            return Response({"status": "password set"}, status=status.HTTP_200_OK)
+
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    ''' @action(detail=True,methods=['put','patch'], serializer_class=AvatarSerializer)
-    def change_avatar(self, request, pk, filename, format=None):
-        parser_classes = (FileUploadParser,)
-        permission_classes = (IsOwnerOrReadOnly,permissions.IsAuthenticated)
-        
-        file_obj = request.FILES
-        if 'file' not in request.data:
-            raise ParseError("Empty content")
-        try:
-            profile = UserProfile(user=request.user)
-            profile.save(request.user, file_obj['file'])
-        except Exception as e:
-            raise e
-        return Response(status=status.HTTP_201_CREATED) '''
 
-
-class UserViewSet(viewsets.ModelViewSet):
-    
-    queryset=User.objects.all()
-    serializer_class=UserSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
-                            IsSameUserAllowEditionOrReadOnly,)
-    
-class GetUserProfileView(viewsets.ModelViewSet): 
-    queryset=UserProfile.objects.all()
-    serializer_class=UserProfileSerializer
-    permission_classes=(IsOwnerOrReadOnly,)
+class GetUserProfileView(viewsets.ModelViewSet):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+    permission_classes = (IsOwnerOrReadOnly,)
 
     def get_queryset(self):
         if self.action == 'list':
             return self.queryset.filter(user_id=self.request.user.id)
         return self.queryset
-   
